6efe75b072f671f3eb107898aa3314448049305f
Daniel Plotnikov <daniilrew@gmail.com>
2024-10-02 21:10:13 +0500

    add graph resampler

--- controllers/freq_drawer.py
+++ controllers/freq_drawer.py
@@ -1,7 +1,10 @@
 import numpy as np
 import plotly.graph_objects as go
+from plotly_resampler import register_plotly_resampler
 
 def get_fig(freq_lst, full_wave, scale_factor=75):
+    register_plotly_resampler(mode='auto')
+
     full_wave = np.interp(
         np.arange(0, len(full_wave), scale_factor),
         np.arange(0, len(full_wave)),
@@ -17,7 +20,7 @@ def get_fig(freq_lst, full_wave, scale_factor=75):
             (duration / sum([b for a, b in freq_lst])) * len(full_wave)
         )
 
-        fig.add_scatter(
+        fig.add_scattergl(
             x=[prev_limit + i for i in range(element_counter)],
             y=full_wave[prev_limit : prev_limit + element_counter],
             name=f"f = {frequency} Hz, t = {int(duration*1000)} ms",
@@ -25,7 +28,7 @@ def get_fig(freq_lst, full_wave, scale_factor=75):
         prev_limit += element_counter
 
     fig.update_layout(
-        title="График проигранного сигнала",
+        title="Упрощенный график проигранного сигнала",
         xaxis_title="t, ms",
         legend_title="Сигналы",
     )
